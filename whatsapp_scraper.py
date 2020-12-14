c3f53b49e427da0e4039fa39194a9824404d1bd6
Aman Jain <amanjain5221@gmail.com>
2020-12-14 22:21:07 +0530

    Update

--- whatsapp_scraper.py
+++ whatsapp_scraper.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import time
 
 from selenium import webdriver
@@ -9,7 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def get_contacts():
     # Return list of numbers
-    return ["91XXXXXXXXXX", "919416046768"]
+    return ["91....", "91..."]
 
 
 if __name__ == "__main__":
@@ -20,26 +21,52 @@ if __name__ == "__main__":
                         help="Specify the chrome driver path")
     parser.add_argument("--message", required=True,
                         help="Specify the message you want to send to your contacts")
+    parser.add_argument("--file_type", required=True,
+                        help="Specify the file type [image/document]")
     args = parser.parse_args()
     chrome_driver = args.chrome_driver
+    file_type = args.file_type
     message = args.message
 
     options = webdriver.ChromeOptions()
-    options.add_argument("--user-data-dir=./newprofile")
+    options.add_argument("--user-data-dir=./newprofile1")
     driver = webdriver.Chrome(chrome_driver, options=options)
     wait = WebDriverWait(driver, 10)
-    driver.get("https://web.whatsapp.com/")
+    # driver.get("https://web.whatsapp.com/")
+
+    media_file_path = os.path.abspath("media/passport-mompdf.pdf")
+    time.sleep(2)
 
     contacts = get_contacts()
     for contact in contacts:
         try:
             driver.get('https://web.whatsapp.com/send?phone={phone}'.format(phone=contact))
-            driver.implicitly_wait(10)
+            driver.implicitly_wait(20)
             driver.find_element_by_xpath(
-                '//div[@class="_3FRCZ copyable-text selectable-text"][@contenteditable="true"][@data-tab="1"]').send_keys(
+                '//div[@class="_1awRl copyable-text selectable-text"][@contenteditable="true"][@data-tab="6"]').send_keys(
                 message + Keys.ENTER)
+            driver.implicitly_wait(20)
+            # driver.switch_to.alert.accept()
+
+            time.sleep(3)
+            attachment = driver.find_element_by_xpath('//div[@title="Attach"]')
+            attachment.click()
+            if file_type == "image":
+                attachment = driver.find_element_by_xpath('//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+                attachment.send_keys(media_file_path)
+            elif file_type == "document":
+                attachment = driver.find_element_by_xpath(
+                    '//input[@accept="*"][@type="file"]')
+                attachment.send_keys(media_file_path)
+
+            time.sleep(3)
+
+            send = driver.find_element_by_xpath('//span[@data-icon="send"]')
+            send.click()
+            # Need to wait until message is sent
             time.sleep(5)
-            print("Message sent to ", contact)
+
+            print("Message sent to", contact)
         except NoSuchElementException:
             print("Failed to send message. Please check if the contact has whatsapp id ")
             continue
